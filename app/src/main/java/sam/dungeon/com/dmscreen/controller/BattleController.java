93ef32efe3e173570331e23d36770929823d1303
pavel novak <didop@atlas.cz>
2017-12-06 21:49:30 +0100

    Added few starting classes

---
+++ app/src/main/java/sam/dungeon/com/dmscreen/controller/BattleController.java
@@ -0,0 +1,8 @@
+package sam.dungeon.com.dmscreen.controller;
+
+/**
+ * Created by Sam on 6.12.2017.
+ */
+
+public class BattleController {
+}
---
+++ app/src/main/java/sam/dungeon/com/dmscreen/model/Monster.java
@@ -0,0 +1,8 @@
+package sam.dungeon.com.dmscreen.model;
+
+/**
+ * Created by Sam on 6.12.2017.
+ */
+
+public class Monster {
+}
